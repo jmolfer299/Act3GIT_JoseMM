b61ec4203e905eb026d1b5ab5df4f4cadf60a656
Jose Maria Molina Fernandez-Crehuet <jmolfer299@g.educaand.com>
2024-11-20 09:32:46 +0100

    Modificamos el programa

--- Act3GIT_JoseMM/src/Act3GIT_JoseMM/Act32GIT_JoseMM.java
+++ Act3GIT_JoseMM/src/Act3GIT_JoseMM/Act32GIT_JoseMM.java
@@ -8,28 +8,63 @@ public class Act32GIT_JoseMM {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Introduce 2 valores");
+		System.out.println("Introduce 3 valores");
 		int num1 = Integer.parseInt(scanner.nextLine());
 		int num2 = Integer.parseInt(scanner.nextLine());
-		
+		int num3 = Integer.parseInt(scanner.nextLine());
 		if(num1>num2)
 		{
-			while(num1>num2)
+			if((num3>num2)&&(num3<num1))
 			{
-				System.out.println(num2);
-				num2 = num2 + 7;
+				while(num3>num2)
+				{
+					System.out.println(num2);
+					num2 = num2 + 7;
+				}
+				System.out.println(num3);
+				while(num1>num2)
+				{
+					System.out.println(num2);
+					num2 = num2 + 7;
+				}
+				System.out.println(num1);
 			}
-			System.out.println(num1);
+			else
+			{
+				while(num1>num2)
+				{
+					System.out.println(num2);
+					num2 = num2 + 7;
+				}
+				System.out.println(num1);
+			}
 		}
 		else if(num2>num1)
 		{
-			while(num2>num1)
+			if((num3>num1)&&(num3<num2))
 			{
-				System.out.println(num1);
-				num1 = num1+7;
+				while(num3>num1)
+				{
+					System.out.println(num1);
+					num1 = num1 + 7;
+				}
+				System.out.println(num3);
+				while(num2>num1)
+				{
+					System.out.println(num1);
+					num1 = num1 + 7;
+				}
+				System.out.println(num2);
 			}
-			System.out.println(num2);
+			else
+			{
+				while(num1>num2)
+				{
+					System.out.println(num2);
+					num2 = num2 + 7;
+				}
+				System.out.println(num1);
+			}
 		}
 	}
-
 }
