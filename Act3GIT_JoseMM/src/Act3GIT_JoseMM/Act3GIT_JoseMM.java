fbde7dfd563eaec9ce18af242d9bdd824e2fbc7a
Jose Maria Molina Fernandez-Crehuet <jmolfer299@g.educaand.com>
2024-11-20 10:04:41 +0100

    Versión final del pryecto

--- Act3GIT_JoseMM/src/Act3GIT_JoseMM/Act3GIT_JoseMM.java
+++ Act3GIT_JoseMM/src/Act3GIT_JoseMM/Act3GIT_JoseMM.java
@@ -5,10 +5,10 @@ public class Act3GIT_JoseMM {
 
 	public static void main(String[] args) {
 		
-		int[] numeros = new int[20];
+		int[] numeros = new int[30];
 		Random random = new Random();
 		
-		for(int i = 0;i<20;i++)
+		for(int i = 0;i<30;i++)
 		{
 			numeros[i]=random.nextInt(10)+1;
 			System.out.print((i+1) + ": "+numeros[i]+"| ");
